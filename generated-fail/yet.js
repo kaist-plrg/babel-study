d62a6de186aec83c84ff1cdb45e806b07411baf1
DJ <f52985@gmail.com>
2022-09-26 16:27:18 +0900

    Add new graal bug

--- generated-fail/yet.js
+++ generated-fail/yet.js
@@ -7,3 +7,8 @@ let x , { } = x += x => { } ;
 // simplest: class C extends (()=>{C})() {}
 class x extends class extends new new new new function ( ) { throw x ; } { ; } { }
 class x extends class extends new new new new function ( ) { x ; } { ; } { }
+
+// Should throw type error when
+// the second argument is 0.
+// https://tc39.es/ecma262/#sec-object.setprototypeof
+Object.setPrototypeOf.call(0, 0)
