82f46cb1bc51cd00eea0de9bbba6beae999c89f6
DJ <f52985@gmail.com>
2022-09-27 13:01:14 +0900

    Remove toString of function

--- generated-fail/yet.js
+++ generated-fail/yet.js
@@ -1,8 +1,3 @@
-// engines does not follow correct semantics of
-// toString for arrow functions
-// https://tc39.es/ecma262/#sec-function.prototype.tostring
-let x , { } = x += x => { } ;
-
 // order between class decl and evaluating extendee expression
 // simplest: class C extends (()=>{C})() {}
 class x extends class extends new new new new function ( ) { throw x ; } { ; } { }
