06ab64dff42aa48e2da41b5ea1c4a7828376bcb9
DJ <f52985@gmail.com>
2022-09-27 16:01:41 +0900

    Add new babel bug about rest and sparse array

--- generated-trans-fail/yet.js
+++ generated-trans-fail/yet.js
@@ -1,2 +1,7 @@
 // body of for loop is executed after transpilation
 for ( var x in function * ( ) { } ) while ( x ) ;
+
+// Array rest assignment pattern with sparse array
+// Original code should have every index as property despite being empty,
+// but the transpield code does not.
+var [ ... x ] = [ , ] ;
